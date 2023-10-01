c273f16596cb7f712cb1ea2bd14d37afcdec0c91
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-01 12:04:35 +0530

    Update InputForm.js
    Added handleInput() function which lifts the entered Input back to the App component

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,12 +1,18 @@
 import "./InputForm.css";
 
-export default function InputForm(){
+export default function InputForm(props){
 
+    function handleSubmit(event){
+        event.preventDefault()
+        const inputText = event.target.text;
+        console.log(inputText.value);
+        props.onAdd(inputText.value);
+    }
 
     return (
-        <form className="submit--form">
+        <form className="submit--form" onSubmit={handleSubmit}>
             <label className="submit__label">Course Goal</label>
-            <input className="submit__input" type="text"></input>
+            <input className="submit__input" type="text" name="text"></input>
             <button className="submit__button" type="submit">Add Goal</button>
         </form>
     )
