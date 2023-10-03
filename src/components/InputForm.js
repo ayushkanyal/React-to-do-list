5723998efbdf320c17581a74041a5cfc5af27898
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-03 11:51:33 +0530

    Update InputForm.js
    Added Javascript logic to handle list additions and CSS modules to establish scope of individual css styling files

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,19 +1,44 @@
-import "./InputForm.css";
+import { useState } from "react";
+import styles from "./Button.module.css"
+import style from './InputForm.module.css'
 
-export default function InputForm(props){
+export default function InputForm(props) {
+  const [isValid, setIsValid] = useState(true);
 
-    function handleSubmit(event){
-        event.preventDefault()
-        const inputText = event.target.text;
-        console.log(inputText.value);
-        props.onAdd(inputText.value);
+  function handleChange() {
+    setIsValid(true);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const inputText = event.target.text;
+    console.log(inputText.value.length);
+    if (inputText.value.length === 0) {
+      setIsValid(false);
+      return;
     }
 
-    return (
-        <form className="submit--form" onSubmit={handleSubmit}>
-            <label className="submit__label">Course Goal</label>
-            <input className="submit__input" type="text" name="text"></input>
-            <button className="submit__button" type="submit">Add Goal</button>
-        </form>
-    )
+    props.onAdd(inputText.value);
+  }
+
+  return (
+    <form  onSubmit={handleSubmit}>
+      <div className={`${style['submit--form']} ${!isValid ? style["invalid"] : ""}`}>
+        <label
+          className="submit__label"
+        >
+          Course Goal
+        </label>
+        <input
+          className="submit__input"
+          onChange={handleChange}
+          type="text"
+          name="text"
+        ></input>
+        <button className={styles.button} type="submit">
+          Add Goal
+        </button>
+      </div>
+    </form>
+  );
 }
