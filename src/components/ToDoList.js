3d70799383614ce333fda8d41d440b482d749f1e
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-03 11:55:16 +0530

    Update ToDoList.js
    Established the scope of CSS files to this js file only by using stlyed-components package.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,10 +1,29 @@
-import "./ToDoList.css"
+import styled from "styled-components";
 
-export default function ToDoList(props){
+const List = styled.ul`
+  list-style-type: none;
+  margin: 20px auto;
+  padding: 0px;
 
-    return(
-        <ul className="toDoList">
-            {props.itemsList.map(item => <li key={item.index} className="toDoList__Item">{item.text}</li>)}
-        </ul>
-    )
+  & > li {
+    width: 35%;
+    margin: auto;
+    margin-bottom: 20px;
+    background-color: rgb(128, 0, 81);
+    padding: 10px;
+    padding-left: 30px;
+    color: white;
+  }
+`;
+
+export default function ToDoList(props) {
+  return (
+    <List>
+      {props.itemsList.map((item) => (
+        <li key={item.index}>
+          {item.text}
+        </li>
+      ))}
+    </List>
+  );
 }
